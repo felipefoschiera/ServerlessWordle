41386579c38e06091a0ec0b961b646fa30454afb
felipefoschiera <felipefoschiera@gmail.com>
2022-01-15 23:12:05 -0300

    Create prop for stack resources

--- serverless-wordle-cdk/lib/props.ts
+++ serverless-wordle-cdk/lib/props.ts
@@ -1,6 +1,11 @@
 import { Table } from "aws-cdk-lib/aws-dynamodb";
 import { Function } from "aws-cdk-lib/aws-lambda";
 
+export interface WordleStackResources {
+    readonly tables: WordleTables;
+    readonly functions: WordleFunctions;
+}
+
 export interface WordleTables {
     readonly game: Table;
     readonly word: Table;
--- serverless-wordle-cdk/lib/serverless-wordle-cdk-stack.ts
+++ serverless-wordle-cdk/lib/serverless-wordle-cdk-stack.ts
@@ -1,14 +1,21 @@
 import { Stack, StackProps } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
+import { WordleStackResources } from './props';
 import { createTables } from './tables';
 import { createFunctions } from './functions';
 
 export class ServerlessWordleCdkStack extends Stack {
+  public readonly resources: WordleStackResources;
+
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    createTables(this);
-    createFunctions(this);
+    const tables = createTables(this);
+    const functions = createFunctions(this);
 
+    this.resources = {
+      tables,
+      functions,
+    };
   }
 }
